878f52f84c7d093924495bbe5f00ada94bde5cab
Philip Crotwell <crotwell@seis.sc.edu>
2017-02-03 15:20:22 -0500

    example

---
+++ example/test.js
@@ -0,0 +1,10 @@
+
+
+//var wp = require('seisplotjs-waveformplot');
+var wp = seisplotjs_waveformplot;
+
+//let filter = require('seisplotjs-filter');
+//filter.doDFT();
+
+wp.createPlotsBySelector('div.myseisplot');
+
